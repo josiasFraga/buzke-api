b62a9834c6049cb3f9e46b72d2e35dbdb211784c
Josias Fraga Demutti <josias@zapshop.com.br>
2021-09-09 15:56:46 -0300

    detalhe do agendamento ok

--- app/Controller/AgendamentosController.php
+++ app/Controller/AgendamentosController.php
@@ -10,6 +10,70 @@ class AgendamentosController extends AppController {
 
     }
 
+    public function index() {
+
+        $this->layout = 'ajax';
+        $dados = $this->request->query;
+        if ( !isset($dados['token']) || $dados['token'] == "" ) {
+            throw new BadRequestException('Dados de usuário não informado!', 401);
+        }
+        if ( !isset($dados['email']) || $dados['email'] == "" ) {
+            throw new BadRequestException('Dados de usuário não informado!', 401);
+        }
+        if ( !isset($dados['agendamento_id']) || $dados['agendamento_id'] == "" ) {
+            throw new BadRequestException('Agendamento não informado!', 401);
+        }
+
+        $token = $dados['token'];
+        $email = $dados['email'];
+        $agendamento_id = $dados['agendamento_id'];
+
+        $dados_token = $this->verificaValidadeToken($token, $email);
+
+        if ( !$dados_token ) {
+            throw new BadRequestException('Usuário não logado!', 401);
+        }
+
+        $this->loadModel('Agendamento');
+
+        $conditions = [
+            'Agendamento.id' => $agendamento_id,
+            'not' => [
+                'Agendamento.cancelado' => 'Y'
+            ]
+        ];
+
+        $agendamento = $this->Agendamento->find('first',[
+            'conditions' => $conditions,
+            'fields' => [
+                'Agendamento.*',
+                'ClienteCliente.id',
+                'ClienteCliente.nome',
+                'ClienteCliente.telefone',
+                'Cliente.id',
+                'Cliente.nome',
+                'ClienteCliente.img',
+                'ClienteCliente.endereco',
+                'ClienteCliente.endreceo_n',
+                'Localidade.loc_no'
+            ],
+            'link' => ['ClienteCliente' => ['Localidade'], 'Cliente']
+        ]);
+
+        if ( count($agendamento) > 0 ) {
+            $agendamento['ClienteCliente']['img'] = $this->images_path . 'clientes_clientes/' . $agendamento['ClienteCliente']['img'];
+            $agendamento['Agendamento']['horario_str'] = date('d/m',strtotime($agendamento['Agendamento']['horario']))." às " . date('H:i',strtotime($agendamento['Agendamento']['horario']));
+            $data_agendamento = date('Y-m-d',strtotime($agendamento['Agendamento']['horario']));
+            if ( $data_agendamento == date('Y-m-d') ) {
+                $agendamento['Agendamento']['horario_str'] = "Hoje às " . date('H:i',strtotime($agendamento['Agendamento']['horario']));
+            }
+        }
+        
+        
+        
+        return new CakeResponse(array('type' => 'json', 'body' => json_encode(array('status' => 'ok', 'dados' => $agendamento))));
+    }
+
     public function empresa() {
 
         $this->layout = 'ajax';
@@ -275,5 +339,99 @@ class AgendamentosController extends AppController {
             $this->sendNotification( $idsNotificacoesCliente, $dados_agendamento_salvo['Agendamento']['id'], "Novo Agendamento :)", "Você tem um novo agendamento de ".$dados_usuario['Usuario']['nome'], "agendamento" );
         
         return new CakeResponse(array('type' => 'json', 'body' => json_encode(array('status' => 'ok', 'msg' => 'Tudo certo! Agendamento cadastrado com sucesso!'))));
+    }
+
+    public function excluir(){
+        $this->layout = 'ajax';
+        //$dados = json_decode($this->request->data['dados']);
+        $dados = $this->request->data['dados'];
+
+        if ( gettype($dados) == 'string' ) {
+            $dados = json_decode($dados);
+            $dados = json_decode(json_encode($dados), true);
+        }
+
+        $dados = (object)$dados;
+
+        if ( !isset($dados->token) || $dados->token == "" ||  !isset($dados->email) || $dados->email == "" || !filter_var($dados->email, FILTER_VALIDATE_EMAIL)) {
+            throw new BadRequestException('Dados de usuário não informado!', 401);
+        }
+
+        if ( !isset($dados->agendamento_id) || $dados->agendamento_id == "" ) {
+            throw new BadRequestException('ID não informado!', 401);
+        }
+
+
+        $dados_usuario = $this->verificaValidadeToken($dados->token, $dados->email);
+
+        if ( !$dados_usuario ) {
+            throw new BadRequestException('Usuário não logado!', 401);
+        }
+
+
+        $this->loadModel('Agendamento');
+
+        $conditions = [
+            'Agendamento.id' => $dados->agendamento_id,
+        ];
+
+        $cancelado_por = 'cliente';
+        if ( $dados_usuario['Usuario']['cliente_id'] != '' ) {
+            $conditions = array_merge($conditions, [
+                'Agendamento.cliente_id' => $dados_usuario['Usuario']['cliente_id']
+            ]);
+        } else {
+            $conditions = array_merge($conditions, [
+                'ClienteCliente.usuario_id' => $dados_usuario['Usuario']['id']
+            ]);
+            $cancelado_por = 'cliente_cliente';
+        }
+
+        $dados_agendamento = $this->Agendamento->find('first',[
+            'fields' => [
+                'Agendamento.id', 
+                'Agendamento.horario', 
+                'ClienteCliente.*',
+                'Cliente.id',
+                'Cliente.nome',
+                'Usuario.id', 
+                'Usuario.nome'
+            ],
+            'conditions' => $conditions,
+            'link' => [
+                'ClienteCliente' => ['Usuario'], 'Cliente'
+            ]
+        ]);
+       
+
+        if ( count($dados_agendamento) == 0 ) {
+            return new CakeResponse(array('type' => 'json', 'body' => json_encode(array('status' => 'erro', 'msg' => 'O agendamento que você está tentando exlcuir, não existe!'))));
+        }
+
+        $dados_salvar['Agendamento']['id'] = $dados_agendamento['Agendamento']['id'];
+        $dados_salvar['Agendamento']['cancelado'] = 'Y';
+
+        if ( !$this->Agendamento->save($dados_salvar) ) {
+            return new CakeResponse(array('type' => 'json', 'body' => json_encode(array('status' => 'erro', 'msg' => 'Ocorreu um erro ao tentar cancelar o agendamento. Por favor, tente mais tarde!'))));
+        }
+
+        //busca os ids do onesignal do usuário a ser notificado do cancelamento do horário
+        $this->loadModel('Token');
+        if ( $cancelado_por == 'cliente' ) {    
+            $notifications_ids = $this->Token->getIdsNotificationsUsuario($dados_agendamento['Usuario']['id']);
+            $nome_usuario_cancelou = $dados_agendamento['Cliente']['nome'];
+        } else {
+            $notifications_ids = $this->Token->getIdsNotificationsUsuario($dados_agendamento['Cliente']['id']);
+            $nome_usuario_cancelou = $dados_agendamento['Usuario']['nome'];
+        }
+
+        if ( count($notifications_ids) > 0 ) {
+            $data_str_agendamento = date('d/m',strtotime($dados_agendamento['Agendamento']['horario']));
+            $hora_str_agendamento = date('H:i',strtotime($dados_agendamento['Agendamento']['horario']));
+            $this->sendNotification( $notifications_ids, $dados_agendamento['Agendamento']['id'], "Agendamento Cancelado :(", $nome_usuario_cancelou." cancelou o agendamento das ".$hora_str_agendamento." do dia ".$data_str_agendamento, "agendamento_cancelado" );
+        }
+    
+        return new CakeResponse(array('type' => 'json', 'body' => json_encode(array('status' => 'ok', 'msg' => 'Agendamento cancelado com sucesso!'))));
+
     }
 }
--- app/Model/Token.php
+++ app/Model/Token.php
@@ -9,4 +9,32 @@ class Token extends AppModel {
 			'foreignKey' => 'usuario_id'
 		)
 	);
+
+	public function getIdsNotificationsUsuario($usuario_id = null) {
+
+		if ( $usuario_id == null )
+			return [];
+		
+		$notifications_ids = $this->find('list',[
+			'fields' => [
+				'Token.id', 'Token.notification_id'
+			],
+			'conditions' => [
+				'Usuario.cliente_id' => $usuario_id,
+				'Token.data_validade >=' => date('Y-m-d') ,
+				'not' => [
+					'Token.notification_id' => null
+				]
+			],
+			'link' => ['Usuario'],
+			'group' => ['Token.notification_id']
+		]);
+
+		if ( count($notifications_ids) > 0 ) {
+			return array_values($notifications_ids);
+		}
+
+		return [];		
+
+	}
 }
